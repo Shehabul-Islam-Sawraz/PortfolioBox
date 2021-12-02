5f93f8b98287f07d419eb2f7da516aaf3d60a836
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-12-02 11:27:01 +0600

    Template & static files linked to model

--- PortfolioBox/settings.py
+++ PortfolioBox/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'PortfolioBox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- user_interface/admin.py
+++ user_interface/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ( User,
+                      InformationModel,
+                      EducationModel,
+                      MessageModel,
+                      ProjectModel,
+                      SkillsetModel,
+                      ExperienceModel )
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(InformationModel)
+admin.site.register(EducationModel)
+admin.site.register(MessageModel)
+admin.site.register(ProjectModel)
+admin.site.register(SkillsetModel)
+admin.site.register(ExperienceModel)
